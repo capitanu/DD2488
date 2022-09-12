172e9f5947453e1ee0c1e4ae97dd0c2ddbfac9a8
Calin Capitanu <calincapitanu@gmail.com>
2022-09-12 13:25:38 +0200

    Different strategy #grading-lab2

--- src/main/scala/punkt0/lexer/Lexer.scala
+++ src/main/scala/punkt0/lexer/Lexer.scala
@@ -9,242 +9,180 @@ object Lexer extends Phase[File, Iterator[Token]] {
 
   def run(f: File)(ctx: Context): Iterator[Token] = {
     val source = scala.io.Source.fromFile(f)
-    val in = source.reader()
-    var char: Int = in.read()
-    var c: Int = 1
-    var l: Int = 1
+    var char: Int = source.next()
 
-    var flag_read = true
+    var temp_pos = source.pos
+    var flag_eof = false
+    var token: Token = new Token(BAD)
 
     new Iterator[Token] {
 
       def read(): Unit = {
-        char = in.read()
-        if(char == '\n') {
-          c = 0
-          l = l + 1
+        if(source.hasNext)
+          char = source.next()
+        else
+          char = -1
+      }
+
+      def skipSingleComment(): Unit = {
+        while(char != '\n' && char != -1)
           read()
-        }
-        else if(char == ' ') {
-          c = c + 1
+      }
+
+      def skipMultiComment(): Unit = {
+        var flag = true
+        while(char != -1 && flag) {
           read()
+          if(char == '*') {
+            read()
+            if(char == '/')
+              flag = false
+          }
         }
-        else {
-          c = c + 1
+        if(char == -1) 
+          Reporter.error("Wrong block comment")
+      }
+
+      def skipNewlineOrWhitespace(): Unit = {
+        while(char == ' ' || char == '\n') {
+          read()
         }
       }
 
-      def hasNext: Boolean = {
-        if(char == -1 && flag_read == false)
-          return false
-        return true
+      def nextMatches(matchChar: Char): Boolean = {
+        read()
+        return matchChar == char
       }
+
+      def stringToken(): Token = {
+        read()
+        var rtn = ""
+        while(char != '\"' && char != -1 && char != '\n') {
+          rtn = rtn + char.toChar
+          read()
+        }
+        if(char != '\"') {
+          read()
+          return new Token(BAD)
+        }
+        else {
+          read()
+          return new STRLIT(rtn)
+        }
+      }
+
+      def intToken(arg: Int): Token = {
+        var rtn = 0
+        while(char <= '9' && char >= '0') {
+          rtn = rtn*10 + (char - 48)
+          read()
+        }
+        return new INTLIT(rtn)
+      }
+
+      def keywordOrIdToken(): Token = {
+        var rtn = ""
+        while((char >= 'A' && char <= 'Z') || (char >= 'a' && char <= 'z') || char == '_' || (char >= '0' && char <= '9')) {
+          rtn = rtn + char.toChar
+          read()
+        }
+        return rtn match {
+          case "object" => new Token(OBJECT)
+          case "class" => new Token(CLASS)
+          case "def" => new Token(DEF)
+          case "override" => new Token(OVERRIDE)
+          case "var" => new Token(VAR)
+          case "Unit" => new Token(UNIT)
+          case "String" => new Token(STRING)
+          case "extends" => new Token(EXTENDS)
+          case "Int" => new Token(INT)
+          case "Boolean" => new Token(BOOLEAN)
+          case "while" => new Token(WHILE)
+          case "if" => new Token(IF)
+          case "else" => new Token(ELSE)
+          case "true" => new Token(TRUE)
+          case "false" => new Token(FALSE)
+          case "this" => new Token(THIS)
+          case "null" => new Token(NULL)
+          case "new" => new Token(NEW)
+          case "println" => new Token(PRINTLN)
+          case _ => new ID(rtn)
+        }
+      }
+
+      def hasNext: Boolean = !flag_eof
 
       def next() = {
 
-        if(!flag_read) {
-          read()
-        } else {
-          if(char != ' ' && char != '\n')
-            flag_read = false
-          else {
-            flag_read = false
-            read()
-          }
-        }
+        skipNewlineOrWhitespace()
 
-        var token: Token = null
+        temp_pos = source.pos
 
         char match {
-          case -1 => token = new Token(EOF).setPos(f, (l << Positioned.COLUMN_BITS) | (c & Positioned.COLUMN_MASK))
-          case ':' => token = new Token(COLON).setPos(f, (l << Positioned.COLUMN_BITS) | (c & Positioned.COLUMN_MASK))
-          case ';' => token = new Token(SEMICOLON).setPos(f, (l << Positioned.COLUMN_BITS) | (c & Positioned.COLUMN_MASK))
-          case '.' => token = new Token(DOT).setPos(f, (l << Positioned.COLUMN_BITS) | (c & Positioned.COLUMN_MASK))
-          case ',' => token = new Token(COMMA).setPos(f, (l << Positioned.COLUMN_BITS) | (c & Positioned.COLUMN_MASK))
+          case -1 => flag_eof = true; token = new Token(EOF).setPos(f, temp_pos)
+          case ':' => read(); token = new Token(COLON).setPos(f, temp_pos)
+          case ';' => read(); token = new Token(SEMICOLON).setPos(f, temp_pos)
+          case '.' => read(); token = new Token(DOT).setPos(f, temp_pos)
+          case ',' => read(); token = new Token(COMMA).setPos(f, temp_pos)
+          case '!' => read(); token = new Token(BANG).setPos(f, temp_pos)
+          case '(' => read(); token = new Token(LPAREN).setPos(f, temp_pos)
+          case ')' => read(); token = new Token(RPAREN).setPos(f, temp_pos)
+          case '{' => read(); token = new Token(LBRACE).setPos(f, temp_pos)
+          case '}' => read(); token = new Token(RBRACE).setPos(f, temp_pos)
+          case '<' => read(); token = new Token(LESSTHAN).setPos(f, temp_pos)
+          case '+' => read(); token = new Token(PLUS).setPos(f, temp_pos)
+          case '-' => read(); token = new Token(MINUS).setPos(f, temp_pos)
+          case '*' => read(); token = new Token(TIMES).setPos(f, temp_pos)
           case '=' =>
-            char = in.read()
-            char match {
-              case '\n' =>
-                token = new Token(EQSIGN).setPos(f, (l << Positioned.COLUMN_BITS) | (c & Positioned.COLUMN_MASK))
-                l = l + 1
-                c = -1
-              case '=' => token = new Token(EQUALS).setPos(f, (l << Positioned.COLUMN_BITS) | (c & Positioned.COLUMN_MASK))
-              case _ =>
-                flag_read = true
-                token = new Token(EQSIGN).setPos(f, (l << Positioned.COLUMN_BITS) | (c & Positioned.COLUMN_MASK))
+            if(nextMatches('=')) {
+              read(); token = new Token(EQUALS).setPos(f, temp_pos)
             }
-            c = c + 1
-          case '!' => token = new Token(BANG).setPos(f, (l << Positioned.COLUMN_BITS) | (c & Positioned.COLUMN_MASK))
-          case '(' => token = new Token(LPAREN).setPos(f, (l << Positioned.COLUMN_BITS) | (c & Positioned.COLUMN_MASK))
-          case ')' => token = new Token(RPAREN).setPos(f, (l << Positioned.COLUMN_BITS) | (c & Positioned.COLUMN_MASK))
-          case '{' => token = new Token(LBRACE).setPos(f, (l << Positioned.COLUMN_BITS) | (c & Positioned.COLUMN_MASK))
-          case '}' => token = new Token(RBRACE).setPos(f, (l << Positioned.COLUMN_BITS) | (c & Positioned.COLUMN_MASK))
-          case '<' => token = new Token(LESSTHAN).setPos(f, (l << Positioned.COLUMN_BITS) | (c & Positioned.COLUMN_MASK))
-          case '+' => token = new Token(PLUS).setPos(f, (l << Positioned.COLUMN_BITS) | (c & Positioned.COLUMN_MASK))
-          case '-' => token = new Token(MINUS).setPos(f, (l << Positioned.COLUMN_BITS) | (c & Positioned.COLUMN_MASK))
-          case '*' => token = new Token(TIMES).setPos(f, (l << Positioned.COLUMN_BITS) | (c & Positioned.COLUMN_MASK))
+            else
+              token = new Token(EQSIGN).setPos(f, temp_pos)
+              
           case '&' =>
-            char = in.read()
+            read()
             char match {
-              case '\n' =>
-                token = new Token(BAD).setPos(f, (l << Positioned.COLUMN_BITS) | (c & Positioned.COLUMN_MASK))
-                l = l + 1
-                c = -1
-              case '&' => token = new Token(AND).setPos(f, (l << Positioned.COLUMN_BITS) | (c & Positioned.COLUMN_MASK))
+              case '&' => read(); token = new Token(AND).setPos(f, temp_pos)
               case _ =>
-                token = new Token(BAD).setPos(f, (l << Positioned.COLUMN_BITS) | (c & Positioned.COLUMN_MASK))
-                flag_read = true
-            }
-            c = c + 1
+                Reporter.error("Expected AND token")
+                token = new Token(BAD).setPos(f, temp_pos)
+            }            
           case '|' =>
-            char = in.read()
+            read()
             char match {
-              case '\n' =>
-                token = new Token(BAD).setPos(f, (l << Positioned.COLUMN_BITS) | (c & Positioned.COLUMN_MASK))
-                l = l + 1
-                c = -1
-              case '|' => token = new Token(OR).setPos(f, (l << Positioned.COLUMN_BITS) | (c & Positioned.COLUMN_MASK))
+              case '|' => read(); token = new Token(OR).setPos(f, temp_pos)
               case _ =>
-                token = new Token(BAD).setPos(f, (l << Positioned.COLUMN_BITS) | (c & Positioned.COLUMN_MASK))
-                flag_read = true
-            }
-            c = c + 1
+                Reporter.error("Expected OR token")
+                token = new Token(BAD).setPos(f, temp_pos)
+            }            
           case '/' =>
-            char = in.read()
+            read()
             char match {
-              case '\n' =>
-                token = new Token(DIV).setPos(f, (l << Positioned.COLUMN_BITS) | (c & Positioned.COLUMN_MASK))
-                l = l + 1
-                c = -1
-              case '/' =>
-                var currLine = l
-                while(currLine == l && char != -1)
-                  read()
-                flag_read = true
-                token = next()
-              case '*' =>
-                var flag = true
-                while(hasNext && flag) {
-                  read()
-                  if(char == -1)
-                    Reporter.error("Wrong token found")
-                  if(char == '*') {
-                    char = in.read()
-                    if(char == '\n') {
-                      l = l + 1
-                      c = 0
-                    }
-                    if(char == '/') {
-                      flag = false
-                    }
-                  }
-                }
-                c = c + 1
-                token = next()
-              case _ =>
-                flag_read = true
-                token = new Token(DIV).setPos(f, (l << Positioned.COLUMN_BITS) | (c & Positioned.COLUMN_MASK))
-                c = c + 1
+              case '/' => skipSingleComment(); token = next()
+              case '*' => skipMultiComment(); token = next()
+              case _ => token = new Token(DIV).setPos(f, temp_pos)
             }
           case _ =>
             if(char <= '9' && char >= '0') {
-              var rtn = 0
-              var tmp_l = l
-              var tmp_c = c
-              while(char <= '9' && char >= '0') {
-                rtn = rtn * 10 + (char.toInt - 48)
-                char = in.read()
-                if(char == '\n') {
-                  l = l + 1
-                  c = 0
-                } else {
-                  c = c + 1
-                }
-              }
-              flag_read = true
-              token = new INTLIT(rtn).setPos(f, (tmp_l << Positioned.COLUMN_BITS) | (tmp_c & Positioned.COLUMN_MASK))
+              token = intToken(char)
+              token.setPos(f, temp_pos)
             }
-            else if((char >= 'A' && char <= 'Z') || (char >= 'a' && char <= 'z')){
-              var tmp_l = l
-              var tmp_c = c
-              var rtn = ""
-              while((char >= 'A' && char <= 'Z') || (char >= 'a' && char <= 'z') || char == '_' || (char >= '0' && char <= '9')) {
-                rtn = rtn + char.toChar
-                char = in.read()
-                if(char == '\n') {
-                  l = l + 1
-                  c = 0
-                } else {
-                  c = c + 1
-                }
-              }
-              flag_read = true
-              token = rtn match {
-                case "object" => new Token(OBJECT).setPos(f, (tmp_l << Positioned.COLUMN_BITS) | (tmp_c & Positioned.COLUMN_MASK))
-                case "class" => new Token(CLASS).setPos(f, (tmp_l << Positioned.COLUMN_BITS) | (tmp_c & Positioned.COLUMN_MASK))
-                case "def" => new Token(DEF).setPos(f, (tmp_l << Positioned.COLUMN_BITS) | (tmp_c & Positioned.COLUMN_MASK))
-                case "override" => new Token(OVERRIDE).setPos(f, (tmp_l << Positioned.COLUMN_BITS) | (tmp_c & Positioned.COLUMN_MASK))
-                case "var" => new Token(VAR).setPos(f, (tmp_l << Positioned.COLUMN_BITS) | (tmp_c & Positioned.COLUMN_MASK))
-                case "Unit" => new Token(UNIT).setPos(f, (tmp_l << Positioned.COLUMN_BITS) | (tmp_c & Positioned.COLUMN_MASK))
-                case "String" => new Token(STRING).setPos(f, (tmp_l << Positioned.COLUMN_BITS) | (tmp_c & Positioned.COLUMN_MASK))
-                case "extends" => new Token(EXTENDS).setPos(f, (tmp_l << Positioned.COLUMN_BITS) | (tmp_c & Positioned.COLUMN_MASK))
-                case "Int" => new Token(INT).setPos(f, (tmp_l << Positioned.COLUMN_BITS) | (tmp_c & Positioned.COLUMN_MASK))
-                case "Boolean" => new Token(BOOLEAN).setPos(f, (tmp_l << Positioned.COLUMN_BITS) | (tmp_c & Positioned.COLUMN_MASK))
-                case "while" => new Token(WHILE).setPos(f, (tmp_l << Positioned.COLUMN_BITS) | (tmp_c & Positioned.COLUMN_MASK))
-                case "if" => new Token(IF).setPos(f, (tmp_l << Positioned.COLUMN_BITS) | (tmp_c & Positioned.COLUMN_MASK))
-                case "else" => new Token(ELSE).setPos(f, (tmp_l << Positioned.COLUMN_BITS) | (tmp_c & Positioned.COLUMN_MASK))
-                case "true" => new Token(TRUE).setPos(f, (tmp_l << Positioned.COLUMN_BITS) | (tmp_c & Positioned.COLUMN_MASK))
-                case "false" => new Token(FALSE).setPos(f, (tmp_l << Positioned.COLUMN_BITS) | (tmp_c & Positioned.COLUMN_MASK))
-                case "this" => new Token(THIS).setPos(f, (tmp_l << Positioned.COLUMN_BITS) | (tmp_c & Positioned.COLUMN_MASK))
-                case "null" => new Token(NULL).setPos(f, (tmp_l << Positioned.COLUMN_BITS) | (tmp_c & Positioned.COLUMN_MASK))
-                case "new" => new Token(NEW).setPos(f, (tmp_l << Positioned.COLUMN_BITS) | (tmp_c & Positioned.COLUMN_MASK))
-                case "println" => new Token(PRINTLN).setPos(f, (tmp_l << Positioned.COLUMN_BITS) | (tmp_c & Positioned.COLUMN_MASK))
-                case _ => new ID(rtn).setPos(f, (tmp_l << Positioned.COLUMN_BITS) | (tmp_c & Positioned.COLUMN_MASK))
-              }
-              
+            else if((char >= 'A' && char <= 'Z') || (char >= 'a' && char <= 'z')) {
+              token = keywordOrIdToken()
+              token.setPos(f, temp_pos)
             }
             else if(char == '\"') {
-              char = in.read()
-              var tmp_l = l
-              var tmp_c = c
-              var rtn = ""
-              if(char == -1 || char == '\n') {
-                token = new Token(BAD).setPos(f, (l << Positioned.COLUMN_BITS) | (c & Positioned.COLUMN_MASK))
-                c = c + 1
-              }
-              else {
-                c = c + 1
-                while(char != '\"' && char != -1) {
-                  rtn = rtn + char.toChar
-                  char = in.read()
-                  if(char == '\n') {
-                    l = l + 1
-                    c = 0
-                  } else {
-                    c = c + 1
-                  }
-                }
-                if(tmp_l == l && char != -1)
-                  token = new STRLIT(rtn).setPos(f, (tmp_l << Positioned.COLUMN_BITS) | (tmp_c & Positioned.COLUMN_MASK))
-                else if(char == -1) {
-                  token = new Token(BAD).setPos(f, (tmp_l << Positioned.COLUMN_BITS) | (tmp_c & Positioned.COLUMN_MASK))
-                  flag_read = true
-                } else {
-                  token = new Token(BAD).setPos(f, (tmp_l << Positioned.COLUMN_BITS) | (tmp_c & Positioned.COLUMN_MASK))
-                }
-
-              }
-            }
-            else {
-              if(char == ' ' || char == '\n')
-                token = next()
-              else
-                token = new Token(BAD).setPos(f, (l << Positioned.COLUMN_BITS) | (c & Positioned.COLUMN_MASK))
+              token = stringToken()
+              token.setPos(f, temp_pos)
+            } else {
+              read(); token = new Token(BAD).setPos(f, temp_pos)
             }
         }
 
         if(token.kind == BAD)
           Reporter.error("Wrong token found")
+
         token
       }
     }
